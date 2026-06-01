f343d79238eed4c22c3b189e4dce25bdb9ec7ddd
CipherNode <support@cipher-node.site>
2026-06-01 19:39:08 +0300

    Fix Tauri desktop web export

---
+++ scripts/patch-tauri-web-export.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const indexPath = path.resolve(process.cwd(), "dist", "index.html");
+
+let html = fs.readFileSync(indexPath, "utf8");
+html = html
+  .replaceAll('href="/', 'href="./')
+  .replaceAll('src="/', 'src="./');
+
+fs.writeFileSync(indexPath, html);
+

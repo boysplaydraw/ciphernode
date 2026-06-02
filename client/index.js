d54a3e5695c8ac773a199b6d1a82643a0006d501
CipherNode <support@cipher-node.site>
2026-06-02 21:58:31 +0300

    Fix web Buffer polyfill bootstrap

--- client/index.js
+++ client/index.js
@@ -2,6 +2,11 @@
 // openpgp ve diğer crypto modülleri yüklenmeden ÖNCE bu kurulmalı
 const { Platform } = require("react-native");
 
+if (Platform.OS === "web") {
+  globalThis.Buffer = globalThis.Buffer || require("buffer").Buffer;
+  globalThis.process = globalThis.process || require("process");
+}
+
 if (Platform.OS !== "web") {
   require("react-native-get-random-values");
   const { install } = require("react-native-quick-crypto");
